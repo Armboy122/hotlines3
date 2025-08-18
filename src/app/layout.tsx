e84e4b8bdfec6d80d06f1eeae9636d13d9d3308a
Armboy122 <arm.09686@gmail.com>
2025-08-18 18:03:46 +0700

    v0.2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
 import Header from "@/component/header";
 import Navbar from "@/component/navbar";
 
@@ -34,7 +33,9 @@ export default function RootLayout({
         <Header/>
 
         {/* Content */}
-        {children}
+        <main className="pt-40">
+          {children}
+        </main>
 
         {/* Navigation */}
         <Navbar/>
